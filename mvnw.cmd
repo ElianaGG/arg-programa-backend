81d3f728620e8e816cc91952baa45d72d6bc4c37
Eliana Ayelén Granado <eligranado43@gmail.com>
2023-05-28 19:16:10 -0500

    Dockerfile

--- mvnw.cmd
+++ mvnw.cmd
@@ -63,7 +63,7 @@ if not "%JAVA_HOME%" == "" goto OkJHome
 echo.
 echo Error: JAVA_HOME not found in your environment. >&2
 echo Please set the JAVA_HOME variable in your environment to match the >&2
-echo location of your Java installation. >&2
+echo location of your installation. >&2
 echo.
 goto error
 
